ab7fd75a03c55fbd8bddb622e671fa3a9accd3aa
Jack <you@example.com>
2020-04-24 19:10:10 +0800

    Initial Commit

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  // 请求域名
+  HTTP_REQUEST_URL:'https://www.dyxgs.cn',
+  // Socket链接
+  WSS_SERVER_URL:'wss://shop.qolai.com/20003',
+  // 请求头
+  HEADER:{
+    'content-type': 'application/json'
+  },
+  // Socket调试模式
+  SERVER_DEBUG:true,
+
+  // 心跳间隔
+  PINGINTERVAL:3000
+}
